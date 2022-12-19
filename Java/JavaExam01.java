0abf5642f7f61ed0d3f1e1205fd31d2c6ae7da04
rlawhdgur <dlsrks_25@naver.com>
2022-12-19 12:28:44 +0900

    updated

---
+++ Java/JavaExam01.java
@@ -0,0 +1,16 @@
+-- 출력하는 법.
+System.out.print("HELLO HUMAN");
+
+System.out.println("HELLO");    // 출력을 한 후 한줄 띄우기.
+System.out.println("HUMAN");
+
+int kor = 100;
+int eng = 90;
+int math = 95;
+System.out.print(kor + eng + math);     // 285
+
+-- 변수 선언.
+int human;      // human이란 변수를 선언.
+human = 100;    // human이란 변수에 100을 대입.
+System.out.println(human);  // 100
+
