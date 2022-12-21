cd6bbe71e1b06a9d05edd1b63911615091f90da1
rlawhdgur <dlsrks_25@naver.com>
2022-12-21 21:09:59 +0900

    updated

---
+++ FrontEnd/03_JAVASCRIPT/EX05/JS/DOM_EXAM_05.js
@@ -0,0 +1,26 @@
+window.addEventListener("load", function() {
+    let section = document.querySelector("#section1");
+    let titleInput = section.querySelector(".title-input");
+    let addButton = section.querySelector("#add-button");
+    let delButton = section.querySelector("#del-button");
+    let menuList = section.querySelector(".menu-list");
+    
+
+    addButton.onclick = function() {
+        let txtNode = document.createTextNode(titleInput.value);
+        // text 박스에 사용자가 입력한 값을 Node로써 생성함.
+
+        menuList.appendChild(txtNode);
+        // 생성돈 Node를 자식으로써 menuList에 등록함.
+
+    }
+    
+    delButton.onclick = function() {
+        let txtNode = menuList.childNodes[0];
+        // menuList 중 첫번째 Node (첫번째 자식)를 txtNode에 등록.
+        menuList.removeChild(txtNode);
+        // menuList에서 등록된 첫번째자식을 자식으로써의 링크를 삭제함.
+    }
+    
+    
+});
---
+++ FrontEnd/README.md
@@ -0,0 +1,10 @@
+# HTML/CSS, JAVA Script, JQuqery
+
+# 수업내용
+- Front_End 준비 : ```Visual Studio Code``` 설치
+- HTML : ```<a></a>```, ```<ul><li></li></ul>```, ```<dl></dl>(dt, dd)```, ```<table></table>```, ```<input>```, ```textarea```, ```select```, ```combobox```,
+        ```radio button```, ```form```, ```method(get, post)```, ```Holy Grail Layout``` 실습
+
+- CSS : ```style```, ```CSS```파일연동, 태그순위, ```selector```태그, ```box```모델, ```style-position```태그, ```container```태그, ```item```태그, ```latout``` 실습
+- JAVA_Script : 변수선언, 배열, JSON으로 변환, ```function```함수선언, ```이벤트```, ```DOM```모델 실습
+        
